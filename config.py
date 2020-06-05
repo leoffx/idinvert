32b85a66bb50f2ec53726038ac077a867a7d7e0a
Pascal Herrmann <pascal.herrmann@tum.de>
2020-06-05 22:20:40 +0200

    Added colab instructions

--- config.py
+++ config.py
@@ -16,37 +16,15 @@ cache_dir = 'cache'
 run_dir_ignore = ['results', 'datasets', 'cache']
 
 #----------------------------------------------------------------------------
+DESC = ""
+GDRIVE_PATH = "/content/gdrive/My Drive/Public/tensorboards_shared/Training_Encoder_TF/128x128_All_Losses" # 128X128 ALL!
 
 RESOLUTION = 128
-DESC = ""
 DATA_DIR = "/content/gdrive/My Drive/Kopie von ffhq-r07.tfrecords"
 PICKLE_DIR = "/content/gdrive/My Drive/Public/tensorboards_shared/Training_Decoder_TF/00000-sgan-ffhq128-1gpu/network-snapshot-011489.pkl"
-
 ENCODER_PICKLE_DIR = ""
-# Encoder
-GDRIVE_PATH = "/content/gdrive/My Drive/Public/tensorboards_shared/Training_Encoder_TF/128x128_All_Losses" # 128X128 ALL!
-#GDRIVE_PATH = "/content/gdrive/My Drive/Public/tensorboards_shared/Training_Encoder_TF/128x128_Only_Rec"   # 128x128 Only Rec!
-#GDRIVE_PATH = "/content/gdrive/My Drive/Public/tensorboards_shared/Training_Encoder_TF/128x128_Weighted"   # 128x128 Weighted!
 
-# Decoder
-#GDRIVE_PATH = "/content/gdrive/My Drive/Public/tensorboards_shared/Training_Decoder_TF/128x128_StyleGAN"
-#GDRIVE_PATH = "/content/gdrive/My Drive/Public/tensorboards_shared/Training_Decoder_TF/64X64_StyleGAN"
-
-# only for decoer
+# only for generator training
 TIME = 0
 KIMG = 0
 
-'''
-
-ToDo:
-* copy data
-* connect GDrive
-* make sure the sub-directories 'images' and 'snapshots' exist
-
-
-
-
-
-Later:
-* automatically create sub-dirs
-'''
