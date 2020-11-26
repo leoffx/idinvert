bef572c4070aaa8c2e7230d98f078a736329a5e3
unknown <leoff152@gmail.com>
2020-11-26 12:13:10 +0100

    Update config.py

--- config.py
+++ config.py
@@ -20,7 +20,7 @@ DESC = ""
 GDRIVE_PATH = "/content/drive/MyDrive/results"
 
 RESOLUTION = 512
-DATA_DIR = "/content/idinvert/datasets/bonsai/bonsai-r09.tfrecords"
+DATA_DIR = "/content/idinvert/datasets/bonsai"
 PICKLE_DIR = "/content/drive/MyDrive/results/network-snapshot-002364.pkl"
 ENCODER_PICKLE_DIR = ""
 
